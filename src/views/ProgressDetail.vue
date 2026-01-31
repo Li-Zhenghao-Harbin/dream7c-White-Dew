aa7f00178d4f3d83d9e63cd5d46780bf8f7e06f7
Li-Zhenghao-Harbin <dream7c@126.com>
2026-01-31 14:35:19 +0800

    表格固定表头

--- src/views/ProgressDetail.vue
+++ src/views/ProgressDetail.vue
@@ -148,6 +148,7 @@
           border
           size="large"
           empty-text="暂无记录"
+          height="calc(100vh - 360px)"
       >
 <!--        <el-table-column prop="companyName" label="公司名称" width="150" fixed />-->
         <el-table-column prop="companyName" label="公司名称" width="150" fixed>
